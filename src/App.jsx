7a6a6eadcc37939bd10680e9f8e4e0a0dca94432
Rohith K S <owlie@Rohiths-MacBook-Pro.local>
2025-05-20 01:45:38 +0530

    chore: set up basic routing structure and install initial packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Console from './pages/Console';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      Init
-    </>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/register" element={<Register></Register>}></Route>
+          <Route path="/console" element={<Console></Console>}></Route>
+        </Routes>
+      </Router>
   )
 }
 
